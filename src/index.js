f2fe3c416a1e8665b77e5630879c75daf9565ee4
harsh mittal <harshmittal1750@gmail.com>
2023-05-19 14:03:38 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -13,16 +13,40 @@ app.use(
 
 app.get("/nonce", function (_, res) {
   res.setHeader("Content-Type", "text/plain");
-  res.send(generateNonce());
+  const generateNonceVar = generateNonce();
+  if (generateNonceVar !== undefined && generateNonceVar !== "") {
+    res.status(200).send(generateNonceVar);
+    console.log(generateNonceVar);
+  } else {
+    res.status(400).send("Error");
+  }
+  // res.status(200).send(generateNonce());
+  // console.log(generateNonce());
 });
+
 const PORT = process.env.PORT || 3001;
 app.post("/verify", async function (req, res) {
+  console.log(
+    "req.body.message: " +
+      req.body.message +
+      " req.body.signature: " +
+      req.body.signature
+  );
   const { message, signature } = req.body;
   const siweMessage = new SiweMessage(message);
+
   try {
-    await siweMessage.verify({ signature });
-    res.send(true);
-  } catch {
+    if (
+      message !== undefined &&
+      message !== "" &&
+      signature !== undefined &&
+      signature !== ""
+    ) {
+      await siweMessage.verify({ signature });
+      res.send(true);
+    }
+  } catch (err) {
+    console.log(err);
     res.send(false);
   }
 });
