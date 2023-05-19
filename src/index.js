f990825027bbb4782adb971922d7f10c6544224b
harsh mittal <harshmittal1750@gmail.com>
2023-05-19 14:05:52 +0530

    handle promise rejection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import cors from "cors";
 import express from "express";
 import { generateNonce, SiweMessage } from "siwe";
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
 app.use(express.json());
 
 app.use(
@@ -11,47 +13,66 @@ app.use(
   })
 );
 
-app.get("/nonce", function (_, res) {
-  res.setHeader("Content-Type", "text/plain");
-  const generateNonceVar = generateNonce();
-  if (generateNonceVar !== undefined && generateNonceVar !== "") {
-    res.status(200).send(generateNonceVar);
-    console.log(generateNonceVar);
-  } else {
-    res.status(400).send("Error");
-  }
-  // res.status(200).send(generateNonce());
-  // console.log(generateNonce());
-});
-
-const PORT = process.env.PORT || 3001;
-app.post("/verify", async function (req, res) {
-  console.log(
-    "req.body.message: " +
-      req.body.message +
-      " req.body.signature: " +
-      req.body.signature
-  );
-  const { message, signature } = req.body;
-  const siweMessage = new SiweMessage(message);
-
+app.get("/nonce", async (req, res) => {
   try {
-    if (
-      message !== undefined &&
-      message !== "" &&
-      signature !== undefined &&
-      signature !== ""
-    ) {
-      await siweMessage.verify({ signature });
-      res.send(true);
-    }
-  } catch (err) {
-    console.log(err);
-    res.send(false);
+    res.setHeader("Content-Type", "application/json");
+
+    const nonce = await generateNonce();
+
+    res.status(200).json({
+      status: true,
+      data: nonce,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      error: error,
+    });
   }
 });
-app.use("*", (req, res) => res.send("Hello World"));
+
+app.post("/verify", async (req, res) => {
+  try {
+    const { message, signature } = req.body;
+    const siweMessage = new SiweMessage(message);
+    const data = await siweMessage.verify({ signature });
+
+    res.setHeader("Content-Type", "application/json");
+
+    res.status(200).json({
+      status: true,
+      data: "something",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      error: error,
+    });
+  }
+});
+
+app.use("*", (req, res) => {
+  res.status(202).json({
+    message: "Welcome to Replicare",
+  });
+});
+
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting Down Server due to Uncaught Exception");
+  process.exit(1);
+});
 
 app.listen(PORT, () => {
-  console.log("connect");
+  console.log("connected to http://localhost:3001");
+});
+
+//Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting Down Server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
